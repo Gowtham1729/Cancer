49a31ee7b717cf4bede1cbfd2e5d976e4fcd06a5
Gowtham1729 <chitipolu.gowtham@iitgn.ac.in>
2019-11-11 15:05:30 +0530

    removed ensemble

--- health/cancer_estimator.py
+++ health/cancer_estimator.py
@@ -9,6 +9,7 @@ TRAINED_MODEL_PATH_2 = '/code/final_model_2.h5'
 TRAINED_MODEL_PATH_3 = '/code/final_model_3.h5'
 TRAINED_MODEL_PATH_4 = '/code/final_model_4.h5'
 TRAINED_MODEL_PATH_5 = '/code/final_model_5.h5'
+TRAINED_MODEL_PATH = '/code/model_problem.h5'
 
 
 def predict_cancer(file_path):
@@ -17,17 +18,20 @@ def predict_cancer(file_path):
     embedding = np.array(extract_embeddings(BIO_BERT_PATH, x))
     embedding = np.mean(embedding, axis=1)
 
-    model = load_model(TRAINED_MODEL_PATH_1)
-    y_pred_1 = model.predict(embedding)
-    model = load_model(TRAINED_MODEL_PATH_2)
-    y_pred_2 = model.predict(embedding)
-    model = load_model(TRAINED_MODEL_PATH_3)
-    y_pred_3 = model.predict(embedding)
-    model = load_model(TRAINED_MODEL_PATH_4)
-    y_pred_4 = model.predict(embedding)
-    model = load_model(TRAINED_MODEL_PATH_5)
-    y_pred_5 = model.predict(embedding)
+    # model = load_model(TRAINED_MODEL_PATH_1)
+    # y_pred_1 = model.predict(embedding)
+    # model = load_model(TRAINED_MODEL_PATH_2)
+    # y_pred_2 = model.predict(embedding)
+    # model = load_model(TRAINED_MODEL_PATH_3)
+    # y_pred_3 = model.predict(embedding)
+    # model = load_model(TRAINED_MODEL_PATH_4)
+    # y_pred_4 = model.predict(embedding)
+    # model = load_model(TRAINED_MODEL_PATH_5)
+    # y_pred_5 = model.predict(embedding)
+    #
+    # y_pred = (y_pred_1 + y_pred_2 + y_pred_3 + y_pred_4 + y_pred_5) / 3
 
-    y_pred = (y_pred_1 + y_pred_2 + y_pred_3 + y_pred_4 + y_pred_5) / 3
+    model = load_model(TRAINED_MODEL_PATH)
+    y_pred = model.predict(embedding)
     cancer_prob = int(y_pred[0][0] * 100)
     return cancer_prob
