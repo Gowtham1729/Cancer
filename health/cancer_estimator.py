7c24a328f5a9aebfe56b311745baa676db474ab9
Gowtham1729 <chitipolu.gowtham@iitgn.ac.in>
2019-11-11 14:39:20 +0530

    ensemble models

--- health/cancer_estimator.py
+++ health/cancer_estimator.py
@@ -3,22 +3,31 @@ from keras_bert import extract_embeddings
 from tensorflow.keras.models import load_model
 import keras.backend.tensorflow_backend as tb
 
-
 BIO_BERT_PATH = '/code/biobert_v1.1_pubmed'
-TRAINED_MODEL_PATH = '/code/model_problem.h5'
+TRAINED_MODEL_PATH_1 = '/code/model_1.h5'
+TRAINED_MODEL_PATH_2 = '/code/model_2.h5'
+TRAINED_MODEL_PATH_3 = '/code/model_3.h5'
+TRAINED_MODEL_PATH_4 = '/code/model_4.h5'
+TRAINED_MODEL_PATH_5 = '/code/model_5.h5'
 
 
 def predict_cancer(file_path):
-
     tb._SYMBOLIC_SCOPE.value = True
     x = np.array([open(file_path, 'r').read()])
     embedding = np.array(extract_embeddings(BIO_BERT_PATH, x))
     embedding = np.mean(embedding, axis=1)
-    model = load_model(TRAINED_MODEL_PATH)
-    y_pred = model.predict(embedding)
-    cancer_prob = int(y_pred[0][0]*100)
-    if cancer_prob < 50:
-        cancer_percentage = int(cancer_prob/10)
-    else:
-        cancer_percentage = (cancer_prob - 50)*2
-    return cancer_percentage
+
+    model = load_model(TRAINED_MODEL_PATH_1)
+    y_pred_1 = model.predict(embedding)
+    model = load_model(TRAINED_MODEL_PATH_2)
+    y_pred_2 = model.predict(embedding)
+    model = load_model(TRAINED_MODEL_PATH_3)
+    y_pred_3 = model.predict(embedding)
+    model = load_model(TRAINED_MODEL_PATH_4)
+    y_pred_4 = model.predict(embedding)
+    model = load_model(TRAINED_MODEL_PATH_5)
+    y_pred_5 = model.predict(embedding)
+
+    y_pred = (y_pred_1 + y_pred_2 + y_pred_3 + y_pred_4 + y_pred_5) / 3
+    cancer_prob = int(y_pred[0][0] * 100)
+    return cancer_prob
